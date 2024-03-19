128eb1c8da74e8b2f43f90bd47a3710440585101
최수원 <suwon3274@gmail.com>
2024-03-19 18:12:21 +0900

    programmers - 카카오프렌즈 컬러링북

---
+++ suwon3274/programmers/카카오프렌즈_컬러링북/Solution.java
@@ -0,0 +1,16 @@
+package 카카오프렌즈_컬러링북;
+
+public class Solution {
+
+    public int[] solution(int m, int n, int[][] picture) {
+
+
+        int numberOfArea = 0;
+        int maxSizeOfOneArea = 0;
+        int[] answer = new int[2];
+        answer[0] = numberOfArea;
+        answer[1] = maxSizeOfOneArea;
+        return answer;
+    }
+
+}

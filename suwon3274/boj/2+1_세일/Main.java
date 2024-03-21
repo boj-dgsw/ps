647b3fd1ee4c733104bcf4f304bb674c85991965
최수원 <suwon3274@gmail.com>
2024-03-21 11:10:52 +0900

    boj - 2+1 세일

---
+++ suwon3274/boj/2+1_세일/Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer[] items = new Integer[n];
+
+        for(int i = 0; i<n; i++) {
+            items[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(items, Collections.reverseOrder());
+
+        long result = 0;
+        for(int i = 0; i<n; i++) {
+            if(i%3 != 2) {
+                result += items[i];
+            }
+        }
+        System.out.print(result);
+    }
+
+}

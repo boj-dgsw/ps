075c08c0cab13927cb84a1ecb2068e0439b3fe07
최수원 <suwon3274@gmail.com>
2024-03-23 11:29:36 +0900

    boj - 게임을 만든 동준이

---
+++ suwon3274/boj/게임을_만든_동준이/Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] levels = new int[n];
+
+        for(int i = 0; i<n; i++) {
+            levels[i] = Integer.parseInt(br.readLine());
+        }
+
+        int result = 0;
+        for(int i = n - 2; i>-1; i--) {
+            int lastLevel = levels[i + 1];
+
+            if(lastLevel <= levels[i]) {
+                result += levels[i] - lastLevel + 1;
+                levels[i] = lastLevel - 1;
+            }
+        }
+        System.out.print(result);
+    }
+
+}

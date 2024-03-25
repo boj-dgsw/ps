47994feffc67f39b2e26f785dd05c20ccb176284
최수원 <suwon3274@gmail.com>
2024-03-25 13:38:43 +0900

    boj - 삼각형 만들기

---
+++ suwon3274/boj/삼각형_만들기/Main.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] straws = new int[n];
+
+        for(int i = 0; i<n; i++) {
+            straws[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(straws);
+
+        int first = n - 1;
+        int second = n - 2;
+
+        for(int i = n - 3; i>-1; i--) {
+            if(straws[i] + straws[second] > straws[first]) {
+                System.out.print(straws[first] + straws[second] + straws[i]);
+                return ;
+            } else {
+                first--;
+                second--;
+            }
+        }
+
+        System.out.print(-1);
+    }
+
+}

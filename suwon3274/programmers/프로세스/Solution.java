cf50ed2add6eb27a6a896ad09c0390afef387fe6
최수원 <suwon3274@gmail.com>
2024-03-26 17:35:22 +0900

    programmers - 프로세스

---
+++ suwon3274/programmers/프로세스/Solution.java
@@ -0,0 +1,52 @@
+package 프로세스;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Solution {
+
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        Queue<Process> que = new LinkedList<>();
+
+        for(int i = 0; i<priorities.length; i++) {
+            que.add(new Process(i, priorities[i]));
+        }
+
+        int cnt = 1;
+        while(!que.isEmpty()) {
+            AtomicBoolean canWork = new AtomicBoolean(true);
+            Process process = que.poll();
+
+            que.forEach(p -> {
+                if(p.priority > process.priority) {
+                    canWork.set(false);
+                }
+            });
+
+            if(canWork.get()) {
+                if(process.index == location) {
+                    answer = cnt;
+                }
+                cnt++;
+            } else {
+                que.add(process);
+            }
+        }
+
+        return answer;
+    }
+
+    static class Process {
+
+        int index, priority;
+
+        Process(int index, int priority) {
+            this.index = index;
+            this.priority = priority;
+        }
+
+    }
+
+}

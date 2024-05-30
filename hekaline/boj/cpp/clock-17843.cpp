ae448a332aed75f91cae4b4ffe837ca8663be94c
Hekaline <106314668+Hekaline@users.noreply.github.com>
2024-05-30 19:17:56 +0900

    Update clock-17843.cpp

--- hekaline/boj/cpp/clock-17843.cpp
+++ hekaline/boj/cpp/clock-17843.cpp
@@ -1,5 +1,10 @@
+// Geometry
+// Silver V
+
 #include <iostream>
 #include <algorithm>
+
+// 빠른 입출력
 #define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
 using namespace std;
@@ -17,6 +22,7 @@ int main()
 	
 	while (t--)
 	{
+		// 시, 분, 초 입력
 		int h, m, s;
 		cin >> h >> m >> s;
 		
@@ -35,13 +41,16 @@ ld get_smallest_angle(ld h, ld m, ld s)
 	
 	ld arr[] = { a1, a2, a3 };
 	sort(arr, arr + 3);
-	
+
+	// 정렬한 침들 사이의 각(angle) 3개
 	ld oa = arr[1] - arr[0];
 	ld ob = arr[2] - arr[1];
 	ld oc = 360.0 - arr[2] + arr[0];
-	
+
+	// 각 크기 정렬
 	ld oArr[] = { oa, ob, oc };
 	sort(oArr, oArr + 3);
-	
+
+	// 가장 작은 값 반환
 	return oArr[0];
 }

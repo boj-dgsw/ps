8f6ca37252c8d7f8c25d8321f3d2e5336d2740da
유지민 <ds3111@dgsw.hs.kr>
2024-07-11 12:21:04 +0900

    01402_아무래도이문제는A번난이도인것같다

---
+++ wlals1126/boj/cpp/01402_아무래도이문제는A번난이도인것같다.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    
+    for (int i = 0; i < T; ++i) {
+        cout << "yes" << endl;
+    }
+    
+    return 0;
+}

1bc68ef17728d72ed54b8c409c3bcab28ad24853
Suwang I <106314668+Hekaline@users.noreply.github.com>
2024-09-17 17:01:13 +0900

    Create beautiful_sequence-21605.cpp

---
+++ hekaline/boj/cpp/beautiful_sequence-21605.cpp
@@ -0,0 +1,23 @@
+// Constructive
+// Silver I
+
+#include <iostream>
+#include <bits/stdc++.h>
+#define FAST_IO \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr); \
+    cout.tie(nullptr);
+
+using namespace std;
+
+int main()
+{
+    FAST_IO;
+
+    int n;
+    cin >> n;
+
+    while (--n)
+        cout << "1 -1 ";
+    cout << "-1 1";
+}

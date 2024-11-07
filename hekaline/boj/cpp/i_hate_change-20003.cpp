51c8ad282cffeb4ce3943190e9985396a4bfc4c8
Suwang I <106314668+Hekaline@users.noreply.github.com>
2024-11-07 09:03:10 +0900

    Create i_hate_change-20003.cpp

---
+++ hekaline/boj/cpp/i_hate_change-20003.cpp
@@ -0,0 +1,55 @@
+// math
+// number theory
+// euclidean
+// Silver I
+
+#include <bits/stdc++.h>
+#define FAST_IO \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr); \
+    cout.tie(nullptr);
+
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    FAST_IO;
+    
+    int n;
+    cin >> n;
+
+    long long molecule = 1;
+    long long denominator = 1;
+
+    cin >> molecule >> denominator;
+    {
+        const ll _gcd = gcd(molecule, denominator);
+        molecule /= _gcd;
+        denominator /= _gcd;
+    }
+
+    while (--n)
+    {
+        ll a, b;
+        cin >> a >> b;
+
+        const ll _gcd = gcd(a, b);
+
+        a /= _gcd;
+        b /= _gcd;
+
+        if (denominator % b == 0)
+        {
+            const ll mul = denominator / b;
+
+            a *= mul;
+            b *= mul;
+        }
+
+        molecule = gcd(molecule, a);
+        denominator = lcm(denominator, b);
+    }
+
+    cout << molecule << ' ' << denominator;
+}
